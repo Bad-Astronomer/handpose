eec74f3a9b508c2ee83f2aa1312105c71f89225e
Bad-Astronomer <kedarmakulkarni@gmail.com>
2023-10-18 12:31:16 +0530

    rotation works

--- blender/main.js
+++ blender/main.js
@@ -9,43 +9,6 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 
 let object;
 
-const loader = new GLTFLoader();
-loader.load(
-    `models/handyhand.glb`,
-    function (glb) {
-        object = glb.scene;
-        scene.add(object);
-
-        // VIDEO CODE STARTS HERE
-        // TIME STAMP 22:00
-        let positions_list = [];
-        glb.scene.traverse(function(child){
-            if(child.userData.prop){
-                const transform = new TransformControls(camera, renderer.domElement);
-                transform.mode = "rotate";
-                transform.space = "local";
-                
-                transform.size = 0.3
-                transform.rotationSnap = Math.PI/10;
-
-                transform.showY = false;
-                transform.showZ = false;
-                
-                // TIMESTAMP 26:00
-
-
-            }
-        })
-        // VIDEO CODE ENDS HERE
-    },
-    function (xhr) {
-        console.log('loaded');
-    },
-    function (error) {
-        console.error(error);
-    }
-);
-
 const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById("container3D").appendChild(renderer.domElement);
@@ -72,15 +35,83 @@ const cameraControls = new OrbitControls(camera, renderer.domElement);
 cameraControls.minDistance = 1;
 cameraControls.maxDistance = 5;
 
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-}
-
 window.addEventListener("resize", function () {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-animate();
+let mouse_down = function(e) {
+    cameraControls.enabled = false;
+}
+
+let mouse_up = function(e) {
+    cameraControls.enabled = true;
+}
+
+
+//bones lists: (contains index)
+const DONT_USE = [6, 10, 14, 18]
+const ROOT = 0
+const WRIST = 1
+const THUMB = [2,5] //! removed 3
+const INDEX = [7, 8, 9]
+const MIDDLE = [11, 12, 13]
+const RING = [15,16,17]
+const LITTLE = [19,20,21]
+
+const BONES =  [7, 8, 9, 11, 12, 13, 15, 16, 17, 19, 20, 21]
+
+
+const clock = new THREE.Clock();
+const rotationSpeed = 1 
+let time;
+let rotationAngle;
+const minAngle = -Math.PI / 4;
+const maxAngle = Math.PI / 4; 
+function animate() {
+    requestAnimationFrame(animate);
+    if(camera) {
+        // renderer.render(scene, camera);
+        if(selectedBone) {
+
+            selectedBone.rotation.x = Math.PI/180 * 60;
+            renderer.render(scene, camera);
+        }
+    }
+}
+
+
+const loader = new GLTFLoader();
+let skinnedMesh;
+let selectedBone;
+loader.load(
+    `models/handyhand.glb`,
+    function (glb) {
+        scene.add(glb.scene)
+        
+        skinnedMesh = glb.scene.children[0].children[0]
+        const bones = skinnedMesh.skeleton.bones
+        selectedBone = bones[17];
+        let i = 0
+        setInterval(() => {
+            selectedBone = bones[BONES[i]];
+            i = (i+1)%BONES.length
+        }, 100)
+        // selectedBone = bones[5]
+
+        console.log(selectedBone);
+        bones[2].rotation.x = Math.PI/180 * 0;
+        bones[2].rotation.z = Math.PI/180 * (-45);
+
+        // bones[0].rotation.z = Math.PI/180 * 90;
+
+        animate();
+    },
+    function (xhr) {
+        console.log('loaded');
+    },
+    function (error) {
+        console.error(error);
+    }
+);

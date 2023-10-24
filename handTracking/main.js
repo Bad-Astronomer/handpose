36af04ce8c9d0a065c01e614ebf339c55b0b8389
Bad-Astronomer <kedarmakulkarni@gmail.com>
2023-10-24 20:13:15 +0530

    Working Three Js Integration

--- handTracking/main.js
+++ handTracking/main.js
@@ -1,3 +1,70 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import Stats from 'stats.js';
+
+//* Viewport
+const viewport = document.getElementById("viewport")
+const scene = new THREE.Scene();
+
+//* Camera
+const camera = new THREE.PerspectiveCamera(75, 640 / 480, 0.1, 1000);
+camera.position.z = 2;
+
+//* Renderer
+const renderer = new THREE.WebGLRenderer({ canvas: viewport, antialias: true });
+renderer.setSize(640, 480);
+
+//* Axis
+const axesHelper = new THREE.AxesHelper( 2.5 );
+scene.add( axesHelper );
+
+//* Controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.minDistance = 1.5;
+controls.maxDistance = 2;
+
+//* Stats
+const viewportStats = new Stats()
+viewportStats.showPanel(0)
+document.getElementById("viewport-container").appendChild(viewportStats.dom)
+viewportStats.dom.style.position = "absolute";
+
+//* Landmark spheres (Keypoints 3D)
+let landmarks = [];
+let keypoints3D = [];
+
+for(let i = 0; i < 21; i++){
+  let sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.05, 16, 16),
+    new THREE.MeshBasicMaterial({ color: 0xe1f5d1 })
+  );
+
+  scene.add(sphere);
+  landmarks.push(sphere)
+}
+
+//* Skeleton 2D
+const connections = [
+  [0, 1], [1, 2], [2, 3], [3, 4],   // Thumb
+  [0, 5], [5, 6], [6, 7], [7, 8],   // Index finger
+  [0, 9], [9, 10], [10, 11], [11, 12],   // Middle finger
+  [0, 13], [13, 14], [14, 15], [15, 16],   // Ring finger
+  [0, 17], [17, 18], [18, 19], [19, 20]   // Little finger
+];
+
+//* Skeleton 3D
+const landmark_connections = []
+connections.forEach((connection, index) => {
+  let line = new THREE.Line(
+    new THREE.BufferGeometry().setFromPoints([landmarks[connection[0]].position, landmarks[connection[1]].position]),
+    new THREE.LineBasicMaterial({ color: 0xffffff })
+  );
+  scene.add(line);
+  landmark_connections.push(line);
+})
+
+
+
 const video = document.getElementById("video");
 const overlay = document.getElementById("overlay");
 const ctx = overlay.getContext("2d");
@@ -9,14 +76,6 @@ const size = {
 
 overlay.width = size.width;
 overlay.height = size.height;
-
-const connections = [
-    [0, 1], [1, 2], [2, 3], [3, 4],   // Thumb
-    [0, 5], [5, 6], [6, 7], [7, 8],   // Index finger
-    [0, 9], [9, 10], [10, 11], [11, 12],   // Middle finger
-    [0, 13], [13, 14], [14, 15], [15, 16],   // Ring finger
-    [0, 17], [17, 18], [18, 19], [19, 20]   // Little finger
-];
 
 
 
@@ -57,9 +116,7 @@ video.onloadeddata = function() {
                     color = "aqua";
                 }
 
-                
                 let keypoints = hand.keypoints;
-                console.log(keypoints)
 
                 //* Joints / Keypoints
                 for (let j = 0; j < keypoints.length; j++) {
@@ -80,38 +137,24 @@ video.onloadeddata = function() {
                     ctx.stroke();
                 });
 
+                keypoints3D = hand.keypoints3D;
+                keypoints3D.forEach((keypoint, index) => {
+                  let {x, y, z} = keypoint;
+                  landmarks[index].position.copy(new THREE.Vector3(x*10, y*-10, z*-10))
+                })
+                
+                // console.log(keypoints3D);
+                connections.forEach((connection, index) => {
+                  landmark_connections[index].geometry.setFromPoints([landmarks[connection[0]].position, landmarks[connection[1]].position]);
+                })
+                
             })
         }
+        renderer.render(scene, camera);
 
+        viewportStats.update();
         requestAnimationFrame(predict);
     }
 
     predict();
 }
-
-import * as THREE from 'three';
-
-const viewport = document.getElementById("viewport")
-
-let scene = new THREE.Scene();
-
-let camera = new THREE.PerspectiveCamera(75, 640 / 480, 0.1, 1000);
-camera.position.z = 5;
-
-let renderer = new THREE.WebGLRenderer({ canvas: viewport, antialias: true });
-renderer.setSize(640, 480);
-
-let geometry = new THREE.BoxGeometry(1, 1, 1);
-let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-
-let cube = new THREE.Mesh(geometry, material);
-
-scene.add(cube);
-
-function animate() {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
-}
-animate();
